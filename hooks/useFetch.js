f3213971ae5110c2a804f468bf2b72999e4b3d37
kehindeBankole <bankolek1@gmail.com>
2022-01-11 23:31:03 +0100

    removed token

--- hooks/useFetch.js
+++ hooks/useFetch.js
@@ -18,8 +18,7 @@ export const useFetch = (url) => {
           method: "GET",
           headers: {
             "Content-Type": "application/json",
-            Authorization:
-              "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjM5YmI3NThiLTZkYjAtNGNlNC04MWU4LWRkYTY2YjQ5NmEyOCIsImVtYWlsIjoia2VubnlAeW9wbWFpbC5jb20iLCJ1c2VybmFtZSI6ImtlaGluZGUiLCJkaXNwbGF5TmFtZSI6ImtlaGluZGUiLCJyb2xlIjoidXNlciIsInNlc3Npb25Ub2tlbiI6ImV5SmhiR2NpT2lKSVV6STFOaUlzSW5SNWNDSTZJa3BYVkNKOS5leUpwWkNJNklqTTVZbUkzTlRoaUxUWmtZakF0TkdObE5DMDRNV1U0TFdSa1lUWTJZalE1Tm1FeU9DSXNJbVZ0WVdsc0lqb2lhMlZ1Ym5sQWVXOXdiV0ZwYkM1amIyMGlMQ0oxYzJWeWJtRnRaU0k2SW10bGFHbHVaR1VpTENKa2FYTndiR0Y1VG1GdFpTSTZJbXRsYUdsdVpHVWlMQ0p5YjJ4bElqb2lkWE5sY2lJc0luUnZhMlZ1Vkhsd1pTSTZJbE5sYzNOcGIyNVViMnRsYmlJc0ltbGhkQ0k2TVRZek9UZzBPVFEyTnl3aVpYaHdJam94TmpReU5EUXhORFkzZlEuaUtFbTRMVHV5VlJBZlk0eVhOU1ZSUzZRbEdsT2plWkc5bU9mdFdKcXo1ayIsInRva2VuVHlwZSI6IkF1dGhUb2tlbiIsImlhdCI6MTYzOTg0OTQ2NywiZXhwIjoxNjUwNjQ5NDY3fQ.jIs12X14pOSfvPN52ppAX0PU-urj_olchulyeZWoek4",
+            Authorization: "",
           },
         });
         const data = await response.json();
